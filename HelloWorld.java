40c1a77d6b6aa20b8fa4a611592b5b267a0a099f
Drew Swineherd <39375695+dswineherd@users.noreply.github.com>
2021-08-11 18:56:34 +0200

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+import static java.lang.System.*;
+public class HelloWorld {
+    public static void main(String[] args){
+        String greet = "Hello World from Java";
+        System.out.println(greet);
+        System.out.println("My greet is:" + greet);
+    }
+}
